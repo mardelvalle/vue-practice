d0cad74d449337a6d563c72b967afc1235238f10
Maria Cristina Del Valle <marcristina205@gmail.com>
2018-12-04 21:10:48 -0500

    added database

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: 8081
+  // db: {
+  //   database: process.env.DB_NAME || 'tabletracker',
+  //   user: process.env.DB_USER || 'tabletracker',
+  //   password: process.env.DB_PASS || 'tabletracker',
+  //   options: {
+  //     dialect: process.env.DIALECT || 'sqlite',
+  //     host: process.env.HOST || 'localhost',
+  //     storage: '.tabletracker.sqlite'
+  //   }
+  // }
+}
