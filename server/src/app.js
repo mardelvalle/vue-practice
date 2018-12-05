4859c1a7393f72462dc58cd8513e60283b4f55a9
Maria Cristina Del Valle <marcristina205@gmail.com>
2018-12-04 21:54:08 -0500

    set up mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,22 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+var mongoose = require('mongoose')
+// const MongoClient = require('mongodb').MongoClient
+
+mongoose.connect('mongodb://vuer:vuewing2@ds125684.mlab.com:25684/vue-practice')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('Connection Succeeded')
+})
+console.log(`at ${config.port}`)
+app.listen(process.env.PORT || config.port)
 
 app.get('/status', (req, res) => {
   res.send({
@@ -16,11 +28,16 @@ app.get('/status', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
+  // console.log(res)
+  db.collection('register').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+  })
   res.send({
     message: `${req.body.email} you were registered`
   })
 })
-
+console.log('app ' + app)
 app.get('/posts', (req, res) => {
   res.send(
     [{
@@ -29,5 +46,3 @@ app.get('/posts', (req, res) => {
     }]
   )
 })
-
-app.listen(process.env.PORT || 8081)
